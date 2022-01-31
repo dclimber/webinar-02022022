c939cb5e9931abba459280d68407968e486fed7a
Dastan Abd <dastand.climber@gmail.com>
2022-01-31 16:08:00 +0600

    feat: 3 — Add more code

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,11 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
+from apps.pages import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home_page, name='home'),
 ]
 
 if settings.DEBUG:
