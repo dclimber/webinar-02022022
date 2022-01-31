20a83aad47e8bff33d1e563922679662fabb88e2
Dastan Abd <dastand.climber@gmail.com>
2022-01-31 15:33:11 +0600

    tests: Use unittest and add user story

--- func_tests.py
+++ func_tests.py
@@ -1,7 +1,46 @@
+import unittest
+
 from selenium import webdriver
 
 PATH_TO_CHROME: str = './chromedriver'
-browser = webdriver.Chrome(PATH_TO_CHROME)
-browser.get('http://localhost:8000')
 
-assert 'not found' in browser.title
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome(PATH_TO_CHROME)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_use_a_contact_form_and_is_redirected_later(self):
+        # Яна узнала, что его друг Декс создал себе сайт портфолио.
+        # Она решила его посмотреть и открыла главную страницу.
+        self.browser.get('http://localhost:8000')
+
+        # Она увидела, что в заголовке сайта говорится, что это
+        # портфолио Декса
+        self.assertIn('Портфолио Декса', self.browser.title)
+
+        self.fail('Finish the test!')  # падает — напоминалка нам дописать тест
+
+        # Яна увидела на сайте Декса контактную форму и решила написать ему
+        # через неё, что она посмотрела его сайт.
+
+        # Яна заполнила поле Full name своими ФИО - Алматинская Яна Рфмшатовна
+
+        # Яна заполнила свой емейл: yana@yandex.ru
+
+        # Яна заполнила номер телефона: +77779997777
+
+        # Яна написала сообщение Дексу:
+        # Декс, я посмотрела твой сайт, ты молодец!
+
+        # Яна нажала кнопку Send
+
+        # И её перебросило на страницу «Спасибо»
+
+        # Довольная Яна ушла пить Шампанское.
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
