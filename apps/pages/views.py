019afd454ec10b56a15c600850d2c6d7d62a7569
Dastan Abd <dastand.climber@gmail.com>
2022-01-31 17:06:56 +0600

    feat: 1 — use home.html template

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import render_to_string
 
 
 def home_page(request):
-    return HttpResponse('<html><title>Портфолио Декса</title></html>')
+    template_rendered_to_string = render_to_string('home.html')
+    return HttpResponse(template_rendered_to_string)
