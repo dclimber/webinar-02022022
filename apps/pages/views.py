4404bed317651be3cf5a2b6ff7c5dde8b8c38738
Dastan Abd <dastand.climber@gmail.com>
2022-01-31 17:07:37 +0600

    refactor: 2 — Use render function

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,8 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.template.loader import render_to_string
 
 
 def home_page(request):
-    template_rendered_to_string = render_to_string('home.html')
-    return HttpResponse(template_rendered_to_string)
+    return render(request, 'home.html')
