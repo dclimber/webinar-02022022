5980598088434ff2d2c525a5a1fd8ddd15c3c4eb
Dastan Abd <dastand.climber@gmail.com>
2022-01-31 16:00:41 +0600

    feat: Add first test — 1

---
+++ apps/pages/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from django.urls import resolve
+
+from .views import home_page
+
+
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found_view = resolve('/')
+
+        self.assertEqual(found_view.func, home_page)
